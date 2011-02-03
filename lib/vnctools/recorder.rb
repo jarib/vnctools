04ff6add05d5aacb75ed6c5229d91449ff80a21e
Jari Bakken <jari.bakken@gmail.com>
2011-02-04 00:11:00 +0100

    Update README.

--- lib/vnctools/recorder.rb
+++ lib/vnctools/recorder.rb
@@ -19,7 +19,7 @@ module VncTools
         "-s", @options[:frame_size] || "1024x768",  # frame size
         "-i", "#{display}.0+0,0",                   # display :1
         "-vcodec", @options[:codec] || "mpeg4",     # video codec
-        "-sameq", output            # output
+        "-sameq", output                            # output
       )
 
       @process.io.inherit! if $DEBUG
